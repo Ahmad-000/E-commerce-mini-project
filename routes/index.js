e05e8db9e9a5b6239746f887a75833eb9a54b70d
Ahmad-000 <ahmed.ah1035@gmail.com>
2025-10-31 17:08:20 +0500

    final working

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,54 @@
-const express = require("express")
-const isLoggedIn = require("../middleware/isLoggedIn")
-const router = express.Router()
-const productModel = require("../models/product-model")
-router.get("/" , (req , res) => {
-    let error = req.flash("error")
-    res.render("index" ,{error})
-})
+const express = require("express");
+const isLoggedIn = require("../middleware/isLoggedIn");
+const router = express.Router();
+const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
-router.get("/shop" , isLoggedIn , async (req , res) => {
-    let products = await productModel.find()
-    res.render("shop" , {products})
-})
+router.get("/", (req, res) => {
+  let error = req.flash("error");
+  res.render("index", { error, loggedin: false });
+});
 
+router.get("/shop", isLoggedIn, async (req, res) => {
+  let products = await productModel.find();
+  let sucess = req.flash("sucess");
+  res.render("shop", { products, sucess });
+});
 
+router.get("/addtocart/:id", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  console.log(user);
+  user.cart.push(req.params.id);
+  await user.save();
+  req.flash("sucess", "Added to cart");
+  res.redirect("/shop");
+});
 
-module.exports = router
+router.get("/cart", isLoggedIn, async (req, res) => {
+  try {
+    const user = await userModel
+      .findOne({ email: req.user.email })
+      .populate("cart");
+
+    // Calculate per-item total and attach it to each product
+    const cartItems = user.cart.map((item) => {
+      const bill = Number(item.price) + 20 - Number(item.discount);
+      return { ...item._doc, bill }; // _doc gives plain JS object from Mongoose document
+    });
+
+    const totalBill = user.cart.reduce((acc, item) => {
+      return acc + (Number(item.price) + 20 - Number(item.discount));
+    }, 0);
+
+    res.render("cart", { user, cartItems , totalBill });
+  } catch (err) {
+    console.error(err);
+    res.redirect("/shop");
+  }
+});
+
+router.get("/logout", isLoggedIn, (req, res) => {
+  res.render("shop");
+});
+
+module.exports = router;
